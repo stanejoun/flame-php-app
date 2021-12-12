9296c5752f04df18e59eac0db6050d289b907479
Olivier PRUDENTE <olivier.prudente@gmail.com>
2021-12-12 22:15:11 +0100

    Update

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Stanejoun\OPFramework\Authentication;
 use Stanejoun\OPFramework\Controller;
+use Stanejoun\OPFramework\JsonResponse;
+use Stanejoun\OPFramework\Route;
 
 class Home extends Controller
 {
@@ -11,5 +13,11 @@ class Home extends Controller
 	public function homeAction(): void
 	{
 		$this->render('home', Authentication::GetAuthenticatedUser());
+	}
+
+	#[Route('getDateTimeAction', '/api/home/datetime', 'GET', 'USER')]
+	public function getDateTimeAction(): JsonResponse
+	{
+		return new JsonResponse(['datetime' => date('Y-m-d H:i:s')]);
 	}
 }
