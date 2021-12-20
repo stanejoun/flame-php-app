b5423f44ec9d57285d3862b751495ee668359196
Olivier PRUDENTE <olivier.prudente@gmail.com>
2021-12-20 22:41:44 +0100

    Update

--- config/constants.php
+++ config/constants.php
@@ -6,13 +6,18 @@ use Stanejoun\OPFramework;
 
 define('DOCUMENT_ROOT', filter_input(INPUT_SERVER, 'DOCUMENT_ROOT'));
 define('ROOT', DOCUMENT_ROOT . '../');
+define('APPS', ROOT . 'apps/');
 define('TEMPLATES', ROOT . 'templates/');
+define('VAR', ROOT . 'var/');
 define('CACHES', ROOT . 'var/caches/');
 define('FILES', ROOT . 'var/files/');
-define('JOBS', ROOT . 'var/jobs/');
 define('LOGS', ROOT . 'var/logs/');
-
 define('BASE_URL', OPFramework\Config::Get('protocol') . '://' . OPFramework\Config::Get('host'));
 
 // DEV|PROD|TEST
 OPFramework\Config::$ENVIRONMENT = OPFramework\Config::DEV_ENVIRONMENT;
+//OPFramework\Config::$ENVIRONMENT = OPFramework\Config::PROD_ENVIRONMENT;
+
+// User property
+//OPFramework\User::$USER_IDENTIFIER_PROPERTY = 'username';
+OPFramework\User::$USER_IDENTIFIER_PROPERTY = 'email';
